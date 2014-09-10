a98afe66261466f7dd73c634f4c0d5c71009f1f9
Jonathon Park <Jon@park.com>
2014-09-10 11:52:38 -0700

    changed scrolling jumps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
-	// $('.first').swctallax({
-	// 	jump: 0.1
-	// });
+	$('.first').swctallax({
+		jump: 0.1
+	});
 
-	// $('.second').swctallax({
-	// 	jump: 0.2
-	// });
+	$('.projects').swctallax({
+		jump: 0.05
+	});
 
-	// $('.third').swctallax({
-	// 	jump: 0.05
-	// });
+	$('.coding').swctallax({
+		jump: 0.005
+	});
 
 	$('.swctallax').swctallax({
 		jump: 0.075
