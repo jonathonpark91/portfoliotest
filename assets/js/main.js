a7a8b05edc97f7944ddfbea3f94a06f09780284c
Jonathon Park <Jon@park.com>
2014-09-10 10:28:29 -0700

    added parallax

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	// $('.first').swctallax({
+	// 	jump: 0.1
+	// });
+
+	// $('.second').swctallax({
+	// 	jump: 0.2
+	// });
+
+	// $('.third').swctallax({
+	// 	jump: 0.05
+	// });
+
+	$('.swctallax').swctallax({
+		jump: 0.075
+	});
+
+	$('.last').swctallax({
+		jump: 0.025
+	});
+
+
+
+
+});
